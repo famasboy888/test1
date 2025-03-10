320166dfe5a35357c4b0f3168f177a60a7d2ac99
famasboy888 <famasboy88@gmail.com>
2025-03-10 18:18:30 +0800

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+    },
+  },
+
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

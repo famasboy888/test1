7b1f74ed5b3f320d4e624cc85837e06ec2ab6635
famasboy888 <famasboy88@gmail.com>
2025-03-10 18:12:32 +0800

    Added MongoDb connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+import { exit } from "process";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    exit(1);
+  });

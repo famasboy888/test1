57a7768f1121c1e61f4573b2a5675dac7eb5a583
famasboy888 <famasboy88@gmail.com>
2025-04-08 21:55:51 +0800

    Added initial header search form functionality

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { RootState } from "../types/signin/reduxSignIn";
 
 export default function Header() {
+  const navigate = useNavigate();
   const { currentUser } = useSelector(
     (state: RootState) => state.user || { loading: false, error: null }
   );
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm") ?? "";
+
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, []);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex mx-auto justify-between items-center max-w-6xl p-3">
@@ -18,14 +43,18 @@ export default function Header() {
         </Link>
         <form
           className="bg-slate-100 p-3 rounded-lg flex items-center"
-          action=""
+          onSubmit={handleSearchSubmit}
         >
           <input
+            value={searchTerm}
+            onChange={handleSearchChange}
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/" className="text-slate-700 hover:underline">

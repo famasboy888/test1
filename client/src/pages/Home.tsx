5b66d797bc149e0c6e05938c644825043dacc79c
famasboy888 <famasboy88@gmail.com>
2025-04-30 11:38:56 +0800

    Resolves #1

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -70,7 +70,7 @@ export default function Home() {
     <main>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next place
+          Find your next place!
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
           At Realty Estate, we believe finding your perfect home should feel as
